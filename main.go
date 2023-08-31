d543d72a0e29d78108d7ce527f8b169aa74e78e5
iamarjun <arjunmanoj1995@gmail.com>
2023-08-31 11:00:06 +0530

    Add godotenv package

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import(
+import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	godotenv.Load(".env")
+
 	fmt.Println("Init")
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("Port not found")
+	}
+
+	fmt.Println("Port: $s", portString)
 }
