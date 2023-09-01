90df1f8b35843551a98e83fc2b3f6425873f1375
iamarjun <arjunmanoj1995@gmail.com>
2023-09-01 10:38:28 +0530

    Boilerplate setup for http server

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -12,11 +15,39 @@ func main() {
 
 	godotenv.Load(".env")
 
-	fmt.Println("Init")
 	portString := os.Getenv("PORT")
 
 	if portString == "" {
 		log.Fatal("Port not found")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Println("Server starting at port: %s", portString)
+
+	error := server.ListenAndServe()
+
+	if error != nil {
+		log.Fatal(error)
 	}
 
 	fmt.Println("Port: $s", portString)
